d40ca00b5d8a2ada9493a4d4001678880ad69459
Matt Slater <matthewslater1998@gmail.com>
2020-09-16 14:14:11 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="max-w-container mx-auto px-5">
+		<logo class="w-32 h-32" />
+
+		<h1>Hello, world!</h1>
+	</div>
+</template>
+
+<script>
+	import Logo from '../components/global/Logo';
+
+	export default {
+		components: {
+			Logo,
+		},
+	};
+</script>
