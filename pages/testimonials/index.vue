e31e4119c7292b45211f180edccb0fb6700af050
Matt Slater <matthewslater1998@gmail.com>
2020-09-17 11:42:11 +0100

    Adds testimonials data

---
+++ pages/testimonials/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<section class="border border-blue p-5 overflow-scroll">
+			<span>component: intro</span>
+
+			<div class="mt-4">
+				<pre v-text="$data.model.intro" />
+			</div>
+		</section>
+
+		<section class="border border-blue p-5 overflow-scroll">
+			<span>component: testimonials</span>
+
+			<div class="mt-4">
+				<pre v-text="$data.model.testimonials" />
+			</div>
+		</section>
+
+		<section class="border border-blue p-5 overflow-scroll">
+			<span>component: cta</span>
+
+			<div class="mt-4">
+				<pre v-text="$data.model.cta" />
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+	export default {
+		async asyncData ({ $content }) {
+			return await $content('testimonials').fetch();
+		},
+	};
+</script>
