03689f875feb0ca9267f30d5f724e05330082c74
Matt Slater <matthewslater1998@gmail.com>
2020-09-17 10:57:32 +0100

    Adds about content

---
+++ pages/about/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<section class="border border-blue p-5 overflow-scroll">
+			<span>component: intro</span>
+
+			<div class="mt-4">
+				<pre v-text="$data.model.intro" />
+			</div>
+		</section>
+
+		<page-builder
+			:components="$data.model.copy"
+		/>
+
+		<section class="border border-blue p-5 overflow-scroll">
+			<span>component: cta</span>
+
+			<div class="mt-4">
+				<pre v-text="$data.model.cta" />
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+	export default {
+		async asyncData ({ $content }) {
+			return await $content('about').fetch();
+		},
+	};
+</script>
